006a6d6ea63d26b455c539ae0b429e0b0e3357af
Bence <m.bence05@gmail.com>
2020-09-19 12:58:51 +0200

    aktualizacia status pouzivatela

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/messaging'
 
 const config = {
-  apiKey: "AIzaSyBVtjaL3GUCkBlaLaXvcZasRtsCpi_jFe0",
-  authDomain: "blesk-microservice-app.firebaseapp.com",
-  databaseURL: "https://blesk-microservice-app.firebaseio.com",
-  projectId: "blesk-microservice-app",
-  storageBucket: "blesk-microservice-app.appspot.com",
-  messagingSenderId: "1028412285180",
-  appId: "1:1028412285180:web:5366d8f246737b40bd3779",
-  measurementId: "G-X21Q10RRFQ"
+  apiKey: 'AIzaSyBVtjaL3GUCkBlaLaXvcZasRtsCpi_jFe0',
+  authDomain: 'blesk-microservice-app.firebaseapp.com',
+  databaseURL: 'https://blesk-microservice-app.firebaseio.com',
+  projectId: 'blesk-microservice-app',
+  storageBucket: 'blesk-microservice-app.appspot.com',
+  messagingSenderId: '1028412285180',
+  appId: '1:1028412285180:web:5366d8f246737b40bd3779',
+  measurementId: 'G-X21Q10RRFQ'
 }
 
 firebase.initializeApp(config)
--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 
 const config = {
-  apiKey: "AIzaSyBVtjaL3GUCkBlaLaXvcZasRtsCpi_jFe0",
-  authDomain: "blesk-microservice-app.firebaseapp.com",
-  databaseURL: "https://blesk-microservice-app.firebaseio.com",
-  projectId: "blesk-microservice-app",
-  storageBucket: "blesk-microservice-app.appspot.com",
-  messagingSenderId: "1028412285180",
-  appId: "1:1028412285180:web:5366d8f246737b40bd3779",
-  measurementId: "G-X21Q10RRFQ"
+  apiKey: 'AIzaSyBVtjaL3GUCkBlaLaXvcZasRtsCpi_jFe0',
+  authDomain: 'blesk-microservice-app.firebaseapp.com',
+  databaseURL: 'https://blesk-microservice-app.firebaseio.com',
+  projectId: 'blesk-microservice-app',
+  storageBucket: 'blesk-microservice-app.appspot.com',
+  messagingSenderId: '1028412285180',
+  appId: '1:1028412285180:web:5366d8f246737b40bd3779',
+  measurementId: 'G-X21Q10RRFQ'
 }
 
 firebase.initializeApp(config);
